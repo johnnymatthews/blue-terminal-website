3ca259c10475be4137b09cefc501dbda9fb489fe
John Matthews <john@mohnjatthews.com>
2017-09-13 14:14:43 -0400

    Removed extra style sheet.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+	// Get browser name.
 	function get_browser_name($user_agent) {
 	    if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'opera';
 	    elseif (strpos($user_agent, 'Edge')) return 'edge';
@@ -11,59 +12,31 @@
 	$browser = get_browser_name($_SERVER['HTTP_USER_AGENT']);
 ?>
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html lang="en">
 <head>
   	<meta charset="UTF-8">
   	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>Mohnjatthews by John Matthews</title>
   	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
-  	<!-- <link href="css/style.css" rel="stylesheet"> -->
   	<link rel="icon" href="favicon.png">
 </head>
 <style>
-	/* GLOBAL */
-	body { color: #36ed39; background-color: #1c1c1c; padding: 30px 30px 30px 30px; font-family: "Roboto Mono", monospace; }
-	a { color: #b4ba1b; }
-	p { margin: 0px 0px 0px 0px; }
-	h2, h3, h4, h5, p, a { font-size: 16px; }
-	header, section { padding-bottom: 15px; }
-
-	/* LAYOUT */
-	.single-project { padding-bottom: 10px; }
-
-	/* TABS */
-	.single-tab { padding-left: 40px; }
-	.double-tab { padding-left: 80px; }
-	@media screen and (max-width: 700px) {
-		.single-tab { padding-left: 20px; }
-		.double-tab { padding-left: 40px; }
-	}
-
-	/* FLASHING TEXT */
-	.blinker { animation: blinker 1s linear infinite; }
-	@keyframes blinker {  50% { opacity: 0; } }
-
-	/* COLOUR TITLES */
-	.title { cursor: pointer; }
-	.title-basic { color: black; background-color: white; }
-	.title-man { color: yellow; background-color: #6195e8; }
-	.title-mohn { color: #2c82b7; background-color: #d1bc1d; }
+	body{color:#36ed39;background-color:#1c1c1c;padding:30px;font-family:"Roboto Mono",monospace}a{color:#b4ba1b}p{margin:0}a,h2,h3,h4,h5,p{font-size:16px}header,section{padding-bottom:15px}.single-project{padding-bottom:10px}.single-tab{padding-left:40px}.double-tab{padding-left:80px}@media screen and (max-width:700px){.single-tab{padding-left:20px}.double-tab{padding-left:40px}}.blinker{animation:blinker 1s linear infinite}@keyframes blinker{50%{opacity:0}}.title{cursor:pointer}.title-basic{color:#000;background-color:#fff}.title-man{background-color:#ff0;color:#6195e8}.title-mohn{color:#36ed39;background-color:#1c1c1c;}
+</style>
+<style id="color-scheme-css">
+	.console-user{color:#2c82b7}.console-os{color:#209b23}.console-pwd{color:#d1bc1d}
 </style>
 <body>
-	<style id="color-scheme-css">
-		.console-user { color: #2c82b7; }
-		.console-os { color: #209b23; }
-		.console-pwd { color: #d1bc1d; }
-	</style>
-	<header>
-		<p>Last login: <?= date('D d M Y h:i:s'); ?></p>
-	</header>
  	<section>
- 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--about</span></p>
- 		<p class="single-tab">Hi, I'm John Matthews, and I write code. Contact information is at the bottom, recent projects are in the middle, and the introductory paragraph is right here. Feel free to get in touch if you have a project idea, need a hand with your code, or just fancy a pint.</p>
+ 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews --about</p>
+ 		<br>
+ 		<header>
+ 			<p class="single-tab">Hi, I'm John Matthews, and I write code. Contact information is at the bottom, recent projects are in the middle, and the introductory paragraph is right here. Feel free to get in touch if you have a project idea, need a hand with your code, or just fancy a pint. I sometimes <a href="/blog">blog</a> about things too.</p>
+ 		</header>
   	</section>
 	<section name="projects" class="projects">
-		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--projects</span></p>
+		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews --projects</p>
+		<br>
 		<div class="single-tab single-project">
 			<p><a href="#">Strip Health Cafe</a></p>
 			<p>Website, hosting, and graphic design for a healthy food-cafe based in Manchester UK.</p>
@@ -71,10 +44,6 @@
 		<div class="single-tab single-project">
 			<p><a href="#">The Ninja Report</a></p>
 			<p>Website and hosting for a metal band in the UK.</p>
-		</div>
-		<div class="single-tab single-project">
-			<p><a href="#">Terrible CMS</a></p>
-			<p>Custom CMS development for personal and freelance projects.</p>
 		</div>
 		<div class="single-tab single-project">
 			<p><a href="#">Friendsum</a></p>
@@ -94,13 +63,14 @@
 		</div>
  	</section>
  	<section>
- 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--contact</span></p>
- 		<p id="email-address-reveal" class="single-tab"><a onclick="toggleEmail()"><u>Email Address</u></a></p>
+ 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews --contact</p>
+ 		<br>
+ 		<p id="email-address-reveal" class="single-tab"><a onclick="toggleEmail()"><u>Click for Email</u></a></p>
  		<p class="single-tab"><a href="//www.github.com/mohnjatthews">GitHub</a></p>
  		<p class="single-tab"><a href="//www.linkedin.com/in/mohnjatthews">LinkedIn</a></p>
  	</section>
  	<section>
-		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> <span class="title title-basic" onclick="changeColorScheme('basic')">basic</span> <span class="title title-man" onclick="changeColorScheme('man')">man</span> <span class="title title-mohn" onclick="changeColorScheme('mohn')">mohn</span><span class="blinker"> &#9608;</span></p>
+		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> style <span class="title title-basic" onclick="changeColorScheme('basic')">basic</span> <span class="title title-man" onclick="changeColorScheme('man')">man</span> <span class="title title-mohn" onclick="changeColorScheme('mohn')">mohn</span></p>
  	</section>
 	<script>
         function toggleEmail() {
@@ -116,13 +86,13 @@
         	switch(color_scheme) {
         	    case 'basic':
         	    	console.log('here!');
-        	        code_snippet = 'body { color: black; background-color: white; } .console-user { color: black; } .console-os { color: grey; } .console-pwd { color: black; }';
+        	        code_snippet = '.console-user,body{color:#000}body{background-color:#fff}.console-os{color:grey}.console-pwd{color:#000}';
         	        break;
         	    case 'man':
-        	        code_snippet = 'body { color: #6195e8; background-color: yellow; } .console-user { color: red; } .console-os { color: blue; } .console-pwd { color: white; }';
+        	        code_snippet = 'body{color:#6195e8;background-color:#ff0}.console-user{color:red}.console-os{color:#00f}.console-pwd{color:#fff}';
         	        break;
         	    default:
-        	        code_snippet = 'body{ color: #36ed39; background-color: #1c1c1c; } .console-user { color: #2c82b7; } .console-os { color: #209b23; } .console-pwd { color: #d1bc1d; }';
+        	        code_snippet = 'body{color:#36ed39;background-color:#1c1c1c}.console-user{color:#2c82b7}.console-os{color:#209b23}.console-pwd{color:#d1bc1d}';
         	}
         	console.log('there!');
         	document.getElementById('color-scheme-css').innerHTML = code_snippet;
@@ -130,3 +100,4 @@
     </script>
 </body>
 </html>
+<?= round(filesize("index.php") / 1024, 2) . 'KB loaded in ' . 1 . ' second. <span class="blinker"> &#9608;</span>'; ?>
