8c8f890ed4d6e8a7968e6b04c9ace96985abe9dd
John Matthews <john@mohnjatthews.com>
2017-09-13 12:52:12 -0400

    CSS style toggles.

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php 
-	// Grab the browser that the user is viewing the site on.
-	function get_browser_name($user_agent)
-	{
-	    if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
-	    elseif (strpos($user_agent, 'Edge')) return 'Edge';
-	    elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
-	    elseif (strpos($user_agent, 'Safari')) return 'Safari';
-	    elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
-	    elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
+	function get_browser_name($user_agent) {
+	    if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'opera';
+	    elseif (strpos($user_agent, 'Edge')) return 'edge';
+	    elseif (strpos($user_agent, 'Chrome')) return 'chrome';
+	    elseif (strpos($user_agent, 'Safari')) return 'safari';
+	    elseif (strpos($user_agent, 'Firefox')) return 'firefox';
+	    elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'ie';
 	    return 'browser';
 	}
+	$browser = get_browser_name($_SERVER['HTTP_USER_AGENT']);
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
@@ -18,56 +17,90 @@
   	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>Mohnjatthews by John Matthews</title>
   	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
-  	<link href="css/style.css" rel="stylesheet">
+  	<!-- <link href="css/style.css" rel="stylesheet"> -->
   	<link rel="icon" href="favicon.png">
 </head>
+<style>
+	/* GLOBAL */
+	body { color: #36ed39; background-color: #1c1c1c; padding: 30px 30px 30px 30px; font-family: "Roboto Mono", monospace; }
+	a { color: #b4ba1b; }
+	p { margin: 0px 0px 0px 0px; }
+	h2, h3, h4, h5, p, a { font-size: 16px; }
+	header, section { padding-bottom: 15px; }
+
+	/* LAYOUT */
+	.single-project { padding-bottom: 10px; }
+
+	/* TABS */
+	.single-tab { padding-left: 40px; }
+	.double-tab { padding-left: 80px; }
+	@media screen and (max-width: 700px) {
+		.single-tab { padding-left: 20px; }
+		.double-tab { padding-left: 40px; }
+	}
+
+	/* FLASHING TEXT */
+	.blinker { animation: blinker 1s linear infinite; }
+	@keyframes blinker {  50% { opacity: 0; } }
+
+	/* COLOUR TITLES */
+	.title { cursor: pointer; }
+	.title-basic { color: black; background-color: white; }
+	.title-man { color: yellow; background-color: #6195e8; }
+	.title-mohn { color: #2c82b7; background-color: #d1bc1d; }
+</style>
 <body>
+	<style id="color-scheme-css">
+		.console-user { color: #2c82b7; }
+		.console-os { color: #209b23; }
+		.console-pwd { color: #d1bc1d; }
+	</style>
 	<header>
 		<p>Last login: <?= date('D d M Y h:i:s'); ?></p>
 	</header>
  	<section>
- 		<p><span class="console-user">user</span>@<span class="console-os"><?= strtolower(get_browser_name($_SERVER['HTTP_USER_AGENT'])); ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--about</span></p>
+ 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--about</span></p>
  		<p class="single-tab">Hi, I'm John Matthews, and I write code. Contact information is at the bottom, recent projects are in the middle, and the introductory paragraph is right here. Feel free to get in touch if you have a project idea, need a hand with your code, or just fancy a pint.</p>
   	</section>
 	<section name="projects" class="projects">
-		<p><span class="console-user">user</span>@<span class="console-os"><?= strtolower(get_browser_name($_SERVER['HTTP_USER_AGENT'])); ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--projects</span></p>
+		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--projects</span></p>
 		<div class="single-tab single-project">
-			<p>- <a href="#">Strip Health Cafe</a></p>
-			<p>- Website, hosting, and graphic design for a healthy food-cafe based in Manchester UK.</p>
+			<p><a href="#">Strip Health Cafe</a></p>
+			<p>Website, hosting, and graphic design for a healthy food-cafe based in Manchester UK.</p>
 		</div>
 		<div class="single-tab single-project">
-			<p>- <a href="#">The Ninja Report</a></p>
-			<p>- Website and hosting for a metal band in the UK.</p>
+			<p><a href="#">The Ninja Report</a></p>
+			<p>Website and hosting for a metal band in the UK.</p>
 		</div>
 		<div class="single-tab single-project">
-			<p>- <a href="#">Terrible CMS</a></p>
-			<p>- Custom CMS development for personal and freelance projects.</p>
+			<p><a href="#">Terrible CMS</a></p>
+			<p>Custom CMS development for personal and freelance projects.</p>
 		</div>
 		<div class="single-tab single-project">
-			<p>- <a href="#">Friendsum</a></p>
-			<p>- Custom lorem ipsum generator created during a hackday.</p>
+			<p><a href="#">Friendsum</a></p>
+			<p>Custom lorem ipsum generator created during a hackday.</p>
 		</div>
 		<div class="single-tab single-project">
-			<p>- <a href="#">DM Bespoke Health</a></p>
-			<p>- Website for a personal fitness instructor contact website.</p>
+			<p><a href="#">DM Bespoke Health</a></p>
+			<p>Website for a personal fitness instructor contact website.</p>
 		</div>
 		<div class="single-tab single-project">
-			<p>- <a href="#">LOTR Timer</a></p>
-			<p>- The Lord of the Rings related hackday project.</p>
+			<p><a href="#">LOTR Timer</a></p>
+			<p>The Lord of the Rings related hackday project.</p>
 		</div>
 		<div class="single-tab single-project">
-			<p>- <a href="#">Tithe Barn</a></p>
-			<p>- Website, hosting, and company branding for a bed and breakfast in Cumbria, UK.</p>
+			<p><a href="#">Tithe Barn</a></p>
+			<p>Website, hosting, and company branding for a bed and breakfast in Cumbria, UK.</p>
 		</div>
  	</section>
  	<section>
- 		<p><span class="console-user">user</span>@<span class="console-os"><?= strtolower(get_browser_name($_SERVER['HTTP_USER_AGENT'])); ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--contact</span></p>
- 		<p id="email-address-reveal" class="single-tab"><a onclick="toggleEmail()">I don't like spam.</a></p>
+ 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews <span class="section-colour">--contact</span></p>
+ 		<p id="email-address-reveal" class="single-tab"><a onclick="toggleEmail()"><u>Email Address</u></a></p>
  		<p class="single-tab"><a href="//www.github.com/mohnjatthews">GitHub</a></p>
  		<p class="single-tab"><a href="//www.linkedin.com/in/mohnjatthews">LinkedIn</a></p>
  	</section>
  	<section>
-		<p><span class="console-user">user</span>@<span class="console-os"><?= strtolower(get_browser_name($_SERVER['HTTP_USER_AGENT'])); ?>:</span><span class="console-pwd">~/$</span> <span class="blinker">_</span></p>
+		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> <span class="title title-basic" onclick="changeColorScheme('basic')">basic</span> <span class="title title-man" onclick="changeColorScheme('man')">man</span> <span class="title title-mohn" onclick="changeColorScheme('mohn')">mohn</span><span class="blinker"> &#9608;</span></p>
  	</section>
 	<script>
         function toggleEmail() {
@@ -77,6 +110,22 @@
             } else {
                 document.getElementById("email-address-reveal").innerHTML = '&nbsp;'
             }
+        }
+        function changeColorScheme(color_scheme) {
+        	var code_snippet
+        	switch(color_scheme) {
+        	    case 'basic':
+        	    	console.log('here!');
+        	        code_snippet = 'body { color: black; background-color: white; } .console-user { color: black; } .console-os { color: grey; } .console-pwd { color: black; }';
+        	        break;
+        	    case 'man':
+        	        code_snippet = 'body { color: #6195e8; background-color: yellow; } .console-user { color: red; } .console-os { color: blue; } .console-pwd { color: white; }';
+        	        break;
+        	    default:
+        	        code_snippet = 'body{ color: #36ed39; background-color: #1c1c1c; } .console-user { color: #2c82b7; } .console-os { color: #209b23; } .console-pwd { color: #d1bc1d; }';
+        	}
+        	console.log('there!');
+        	document.getElementById('color-scheme-css').innerHTML = code_snippet;
         }
     </script>
 </body>
