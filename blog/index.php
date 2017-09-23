a08212c56362abad345c29c1b0f0d0b5d65d6712
John Matthews <john@mohnjatthews.com>
2017-09-23 14:00:18 +0100

    Adding titles.

--- blog/index.php
+++ blog/index.php
@@ -16,9 +16,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>Mohnjatthews by John Matthews</title>
+	<title>Mohnjatthews - Blog</title>
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
-	<link href="/style.css" rel="stylesheet">
+	<link href="/assets/css/style.css" rel="stylesheet">
 	<link rel="icon" href="/favicon.png">
 	<style id="color-scheme-css"></style>
 </head>
@@ -33,12 +33,16 @@
 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> mohnjatthews --blog</p>
 		<br>
 		<article class="single-tab single-project">
-			<p><a href="creating-a-lamp-stack-in-vagrant">Creating a LAMP Stack in Vagrant</a></p>
+			<p><a href="posts/a-week-of-hardstyle.php">A Week of Hardstyle</a></p>
+		</article>
+		<article class="single-tab single-project">
+			<p><a href="posts/creating-a-lamp-stack-in-vagrant.php">Creating a LAMP Stack in Vagrant</a></p>
 		</article>
 	</section>
 	<section>
 		<p><span class="console-user">user</span>@<span class="console-os"><?= $browser ?>:</span><span class="console-pwd">~/$</span> style <span class="title title-basic" onclick="changeColorScheme('basic')">basic</span> <span class="title title-man" onclick="changeColorScheme('man')">man</span> <span class="title title-mohn" onclick="changeColorScheme('mohn')">mohn</span></p>
 	</section>
+	<script src="/assets/scripts/email-address-reveal.js"></script>
+	<?= round(filesize(__FILE__) / 1024, 2) . 'KB loaded in ' . 1 . ' second. <span class="blinker"> &#9608;</span>'; ?>
 </body>
 </html>
-<?= round(filesize("index.php") / 1024, 2) . 'KB loaded in ' . 1 . ' second. <span class="blinker"> &#9608;</span>'; ?>
