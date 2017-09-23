7608d7a65e7a990e4e0b6c5b9a170d8dbda39d9e
John Matthews <john@mohnjatthews.com>
2017-09-23 13:40:52 +0100

    Removing console.logs

--- assets/scripts/email-address-reveal.js
+++ assets/scripts/email-address-reveal.js
@@ -6,7 +6,7 @@ function changeColorScheme(o) {
     var e;
     switch (o) {
         case "basic":
-            console.log("here!"), e = ".console-user,body{color:#000}body{background-color:#fff}.console-os{color:grey}.console-pwd{color:#000}";
+            e = ".console-user,body{color:#000}body{background-color:#fff}.console-os{color:grey}.console-pwd{color:#000}";
             break;
         case "man":
             e = "body{color:#6195e8;background-color:#ff0}.console-user{color:red}.console-os{color:#00f}.console-pwd{color:#fff}";
@@ -14,5 +14,5 @@ function changeColorScheme(o) {
         default:
             e = "body{color:#36ed39;background-color:#1c1c1c}.console-user{color:#2c82b7}.console-os{color:#209b23}.console-pwd{color:#d1bc1d}"
     }
-    console.log("there!"), document.getElementById("color-scheme-css").innerHTML = e
+    document.getElementById("color-scheme-css").innerHTML = e
 }
