53b4f27b6d41e3f721c191869f07b927ca4f55da
John Matthews <john@mohnjatthews.com>
2017-10-24 16:08:03 +0100

    Removing background from colour change.

--- assets/scripts/colour-change.js
+++ assets/scripts/colour-change.js
@@ -2,13 +2,13 @@ function changeColorScheme(o) {
     var e;
     switch (o) {
         case "basic":
-            e = ".console-user,body{color:#fff}body{background-color:#fff}.console-os{color:grey}.console-pwd{color:#fff}";
+            e = ".console-user,body{color:#fff}.console-os{color:grey}.console-pwd{color:#fff}";
             break;
         case "man":
-            e = "body{color:#6195e8;background-color:#ff0}.console-user{color:red}.console-os{color:#00f}.console-pwd{color:#fff}";
+            e = "body{color:#6195e8}.console-user{color:red}.console-os{color:#00f}.console-pwd{color:#fff}";
             break;
         default:
-            e = "body{color:#36ed39;background-color:#1c1c1c}.console-user{color:#2c82b7}.console-os{color:#209b23}.console-pwd{color:#d1bc1d}"
+            e = "body{color:#36ed39}.console-user{color:#2c82b7}.console-os{color:#209b23}.console-pwd{color:#d1bc1d}"
     }
     document.getElementById("color-scheme-css").innerHTML = e
 }
