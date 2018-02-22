f999c8a15dad2ffa4dc97d5f67c98c84c85d9927
John Matthews <john@Johns-MacBook-Air.local>
2018-02-22 14:18:45 +0000

    Including blog stuff.

--- blog/index.php
+++ blog/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>mohnjatthews | johnmatthews</title>
+    <title>mohnjatthews | blog</title>
     <style>
         a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}
     
@@ -97,6 +97,10 @@
             margin-top: 15px;
         }
 
+        h2 span {
+            color: yellow;
+        }
+
         p span {
             color: white;
         }
@@ -128,40 +132,43 @@
 <body>
     <nav>
         <ul>
-            <li><a class="active" href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="/blog">Blog</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/#about">About</a></li>
+            <li><a href="/#projects">Projects</a></li>
+            <li><a class="active" href="#">Blog</a></li>
+            <li><a href="/#contact">Contact</a></li>
         </ul>
     </nav>
 
     <main>
         <header>
-            <h1>mohnjatthews.com</h1>
+            <h1>mohnjatthews.com / blog</h1>
         </header>
 
-        <about id="about">
-            <h2>>>> Hi there!</h2>
-            <p class="lead">I'm John Matthews. I write code and technical docs. Contact information is at the bottom, recent projects are in the middle, and the introductory paragraph is right here. Feel free to get in touch if you have a project idea, need a hand with your code, or just fancy a pint.</p>
-        </about>
+        <article>
+            <h2>>>> A Week of Hardstyle <span>12th Feb 2017</span></h2>
+            <p class="lead">I'm John Matthews. I write code and technical docs. Contact information is at the bottom, recent projects are in the middle, and the introductory...</p>
+        </article>
 
-        <projects id="projects">
-            <h2>>>> Projects</h2>
-            <p><a href="//striphealthcafe.com/">striphealthcafe.com</a><br>Website, hosting, and graphic design for a healthy-food cafe based in Manchester UK.</p><br>
-            <p><a href="//theninjareport.co.uk">theninjareport.co.uk</a><br>Website and hosting for a metal band in the UK.</p><br>
-            <p><a href="//mohnjatthews.github.io/friendsum/">friendsum.site</a><br>Custom lorem ipsum generator created during a hackday.</p><br>
-            <p><a href="//davidmollard.co.uk">davidmollard.co.uk</a><br>Website for a personal fitness instructor contact website.</p><br>
-            <p><a href="//mohnjatthews.com/projects/lotr-timer">lotr-timer.site</a><br>The Lord of the Rings related hackday project.</p><br>
-            <p><a href="//www.tithebarn.net/">tithebarn.net</a><br>Website, hosting, and company branding for a bed and breakfast in Cumbria, UK.</p><br>
-        </projects>
+        <article>
+            <h2>>>> Blog</h2>
+            <p class="lead">I'm John Matthews. I write code and technical docs. Contact information is at the bottom, recent projects are in the middle, and the introductory...</p>
+        </article>
 
-        <contact id="contact">
-            <h2>>>> Contact</h2>
-            <p id="email-address-reveal"><a onclick="toggleEmail()">Click for Email</a></p>
-            <p><a href="//github.com/mohnjatthews">GitHub</a></p>
-            <p><a href="//www.linkedin.com/in/mohnjatthews">LinkedIn</a></p>
-        </contact>
+        <article>
+            <h2>>>> Blog</h2>
+            <p class="lead">I'm John Matthews. I write code and technical docs. Contact information is at the bottom, recent projects are in the middle, and the introductory...</p>
+        </article>
+
+        <article>
+            <h2>>>> Blog</h2>
+            <p class="lead">I'm John Matthews. I write code and technical docs. Contact information is at the bottom, recent projects are in the middle, and the introductory...</p>
+        </article>
+
+        <article>
+            <h2>>>> Blog</h2>
+            <p class="lead">I'm John Matthews. I write code and technical docs. Contact information is at the bottom, recent projects are in the middle, and the introductory...</p>
+        </article>
     </main>
 
     <footer>
